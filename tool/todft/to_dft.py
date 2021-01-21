ef77ffcccf4c4463de7f619ceb0d38a534d09a9b
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2021-01-21 23:41:36 +0900

    update to_dft

--- tool/todft/to_dft.py
+++ tool/todft/to_dft.py
@@ -1,217 +1,134 @@
-########## (Edit) Parameters of Quantum Espresso (Edit) ##########
-calculation='relax' # 'scf','realx','bands',...
-ecutwfc=20.0        # Cut-off energy in Ry
-kpts=(3,3,1)        # sampling k points (Monkhorst-Pack grid)
-nbands=33           # # of bands (only used in band structure calc
-pseudo_dir='./'     # Pseudopotential directory
-pseudopotentials={ 'Si': 'Si.pbe-mt_fhi.UPF',
-                   'H' : 'H.pbe-mt_fhi.UPF' }
-##################################################################
-
-'''
-TRHEPD
-
-Usage example:
-  $ python trhepd.py abc.xyz
-
-Output files:
-  abc.cif
-  abc_ext.xyz
-  espresso.pwi
-  espresso.pwo
-'''
-
+import numpy as np
+from ase import Atoms
 import sys
 import math
 
 args = sys.argv
+if len(args) != 2:
+    print("Usage: python to_dft.py <toml file>")
+    exit(0)
+file_name = args[1]
+import toml
+dirs = toml.load(file_name)
+input_main = dirs["Main"]
+input_ase = dirs["ASE"]
+input_sol = dirs["Solver"]
+pseudopotentials = input_sol.pop("pseudo")
+
+import pprint
+print("Parameters for main part")
+pprint.pprint(input_main)
+print("Parameters for ase part")
+pprint.pprint(input_ase)
+print("Parameters for solver part")
+pprint.pprint(input_sol)
+print("Pseudopotentials")
+pprint.pprint(pseudopotentials)
 
 ### Read/Write file names ###
+file_name = input_main["input_xyz_file"]
+wfile_name = input_main["output_file_head"]
 
-file_name = args[1]
+from ase.io.xyz import read_xyz, write_xyz
+atoms = read_xyz(open(file_name))
+atoms_info = Atoms()
+for atom in atoms:
+    atoms_info += atom
+ratoms = atoms_info.get_positions()
+satoms = atoms_info.symbols
+natoms = ratoms.shape[0]
 
-n = len(file_name)
-m = len('.xyz')
-
-base_name = file_name[:n-m]
-
-wfile_name = base_name+'_ext.xyz'
-
-print('Input file name =',file_name)
-print('Output file name =',wfile_name)
-
-### Read ###
-
-f = open(file_name)
-r = f.readlines()    # r is a list constructed with each line of f
-f.close()
-
-### pick the atomic-coordinate lines ###
-
-ratoms = []
-for rl in r:               # rl is a string
-    rs = rl.split()        # rs is a list
-    if len(rs) == 4 and rs[0].isalpha():
-        ratoms.append(rs)  # ratoms is a list of lists
-
-natoms = len(ratoms)
-print('natoms=', natoms)
-
-### convert the atomic coordinates in float ###
-
-z = []
-for rs in ratoms:
-    z.append( float(rs[3]) )
-
-### slab thickness ###
-
-slab_size = max(z) - min(z)
+z_max = np.max(ratoms, axis=0)[2]
+z_min = np.min(ratoms, axis=0)[2]
+slab_size = z_max - z_min
 print('slab_size=', slab_size)
+z_margin = input_main["param"]["z_margin"]
+z_atoms = ratoms[:,2]
 
-### z-coordinate of the bottom-most atoms ###
+z_bottom_most = [z_min - z_margin, z_min + z_margin]
+second_min_index = np.where(np.sort(z_atoms) > z_min)[0][0]
+z_bottom_2nd_val = np.sort(z_atoms)[second_min_index]
+z_bottom_2nd = [z_bottom_2nd_val - z_margin, z_bottom_2nd_val + z_margin]
 
-z_mergin = 0.001
-
-z_bottom_most = min( z )
-z_bottom_most0 = z_bottom_most-z_mergin
-z_bottom_most1 = z_bottom_most+z_mergin
-
-z_bottom_2nd = min([z2 for z2 in z if z2 > z_bottom_most1])
-z_bottom_2nd0 = z_bottom_2nd - z_mergin
-z_bottom_2nd1 = z_bottom_2nd + z_mergin
-
-ratoms_bottom_most = []
-for i in range(natoms):
-    if z_bottom_most0 <= z[i] <= z_bottom_most1:
-        rs = ratoms[i]
-        ratoms_bottom_most.append([rs[0],float(rs[1]),float(rs[2]),float(rs[3])])
-
-ratoms_bottom_2nd = []
-for i in range(natoms):
-    if z_bottom_2nd0 <= z[i] <= z_bottom_2nd1:
-        rs = ratoms[i]
-        ratoms_bottom_2nd.append([rs[0],float(rs[1]),float(rs[2]),float(rs[3])])
+atoms_bottom_most = Atoms()
+for idx in reversed(np.where( (z_atoms <= z_bottom_most[1]) & (z_atoms >= z_bottom_most[0]))[0]):
+    atoms_bottom_most += atoms_info.pop(idx)
+ratoms_bottom_most = atoms_bottom_most.get_positions()
+ratoms_bottom_2nd = ratoms[np.where((z_atoms <= z_bottom_2nd[1]) & (z_atoms >= z_bottom_2nd[0]))]
 
 ### Add Hydrogen terminators ###
-
-r_SiH = 1.48 #angstrom
-theta = 109.5 #H-Si-H angle in degree
+r_SiH = input_main["param"]["r_SiH"]
+theta = input_main["param"]["theta"]
 
 dx = r_SiH * math.sin( math.radians(theta*0.5) )
 dy = dx
 dz = r_SiH * math.cos( math.radians(theta*0.5) )
 
-Hs = []
-for r2 in ratoms_bottom_2nd:
-    dmin=100000.0
-    imin=0
-    for i in range(len(ratoms_bottom_most)):
-        r1 = ratoms_bottom_most[i]
-        x12 = r1[1]-r2[1]
-        y12 = r1[2]-r2[2]
-        d12 = x12**2 + y12**2
-        if d12 < dmin:
-            dmin = d12
-            imin = i
-    r1 = ratoms_bottom_most[imin]
-    x12 = r1[1] - r2[1]
-    y12 = r1[2] - r2[2]
-    if (x12 == 0.0 and y12 != 0.0) or (x12 != 0.0 and y12 == 0.0):
+tmp_x_idx = np.arange(ratoms_bottom_most.shape[0])
+tmp_y_idx = np.arange(ratoms_bottom_2nd.shape[0])
+xx, yy = np.meshgrid(tmp_x_idx, tmp_y_idx)
+rxy_atoms_most = ratoms_bottom_most[:, :2]
+rxy_atoms_2nd = ratoms_bottom_2nd[:, :2]
+distances = np.linalg.norm(rxy_atoms_most[xx]-rxy_atoms_2nd[yy], axis=2)
+
+for idx, r2 in enumerate(ratoms_bottom_2nd):
+    r1 = ratoms_bottom_most[np.argmin(distances[:, idx])]
+    x12 = r1[0] - r2[0]
+    y12 = r1[1] - r2[1]
+    if (np.abs(x12) < 1e-12 and np.abs(y12) > 1e-12) or (np.abs(x12) > 1e-12 and np.abs(y12) < 1e-12):
         print('(001)surface')
-        if x12 == 0.0:
-            Hs.append( ['H','{:f}'.format(r2[1]),'{:f}'.format(r2[2]-dy),'{:f}'.format(r2[3]-dz)] )
-            Hs.append( ['H','{:f}'.format(r2[1]),'{:f}'.format(r2[2]+dy),'{:f}'.format(r2[3]-dz)] )
+        if np.abs(x12) < 1e-12:
+            atoms_info += Atoms(numbers=[1,1], positions=[(r2[0], r2[1] - dy, r2[2] - dz), (r2[0], r2[1] + dy, r2[2] - dz)])
         else:
-            Hs.append( ['H','{:f}'.format(r2[1]-dx),'{:f}'.format(r2[2]),'{:f}'.format(r2[3]-dz)] )
-            Hs.append( ['H','{:f}'.format(r2[1]+dx),'{:f}'.format(r2[2]),'{:f}'.format(r2[3]-dz)] )
-    elif x12==0.0 and y12==0.0 :
+            atoms_info += Atoms(numbers=[1,1], positions=[(r2[0] - dx, r2[1], r2[2] - dz), (r2[0] + dx, r2[1], r2[2] - dz)])
+    elif np.abs(x12) < 1e-12 and np.abs(y12) < 1e-12:
         print('(111)surface')
-        Hs.append( ['H','{:f}'.format(r2[1]),'{:f}'.format(r2[2]),'{:f}'.format(r2[3]-r_SiH)] )
+        atoms_info += Atoms(numbers=[1], positions=[(r2[0], r2[1], r2[2] - r_SiH)])
 
-### atomic coordinates including H terminators ###
-
-n = len(ratoms_bottom_most)
-ratoms_with_H = []
-for rl in ratoms[:natoms-n]:
-    str_rl = '  '.join(rl)+'\n'
-    ratoms_with_H.append(str_rl)
-for rl in Hs:
-    str_rl = '  '.join(rl)+'\n'
-    ratoms_with_H.append(str_rl)
-
-natoms = natoms - n + len(Hs)
-print('natoms=',natoms)
-
+print("Symbols = {}".format(atoms_info.symbols))
+print("natoms = {}".format(len(atoms_info)))
 ### Get cell info ###
 #  The input XYZ file is assumed to have the cell info
 #  at the last two lines
-
-n = len(r)
-a1 = r[n-2].split()
-a2 = r[n-1].split()
-a3 = [ str(slab_size+10.0) ]
-
-### Make the cell info ###
-#  (generate the extended-xyz file)
-
-comment = 'Lattice="'+a1[0]+' 0.0 0.0  0.0 '+a2[1]+' 0.0 '+' 0.0 0.0 '+a3[0]+'"'+' Properties=species:S:1:pos:R:3 pbc="T T T"\n'
-
-f = open(wfile_name, mode='w')
-f.write(str(natoms)+'\n')
-f.write(comment)
-for rl in ratoms_with_H:
-    f.write(rl)
-f.close()
-
-### ASE (Atomic Simulation Environment) ###
-
+a1 = input_main["lattice"]["unit_vec"][0]
+a2 = input_main["lattice"]["unit_vec"][1]
+a3 = np.array([0.0, 0.0, slab_size+input_main["param"]["slab_margin"]])
+atoms_info.set_cell(np.array([a1, a2, a3]))
+comment = "Lattice= \"{} {} {} {} {} {} {} {} {}\" Properties=species:S:1:pos:R:3 pbc=\"T T T\"".format(a1[0], a1[1], a1[2], a2[0], a2[1], a2[2], a3[0], a3[1], a3[2])
+write_xyz(open(wfile_name+".xyz" ,"w"), atoms_info, comment)
 from ase.io import read, write
-from ase.constraints import FixAtoms
-from ase.calculators.espresso import Espresso
-
-a = read(wfile_name)
-cell = a.get_cell()
-
+ase_union = read(wfile_name+".xyz")
 ### CIF file ###
-
-a.write(base_name+'.cif', format='cif')
-
+ase_union.write(wfile_name+'.cif', format='cif')
 ### Constraints ###
-
-z=[]
-for rl in ratoms_with_H:  # rl is a string
-    rs = rl.split()       # rs is a list
-    z.append(float(rs[3]))
-
-c = FixAtoms(indices=[s for s in range(natoms) if z[s]<=z_bottom_2nd1])
-a.set_constraint(c)
+from ase.constraints import FixAtoms
+z_atoms = atoms_info.get_positions()[:,2]
+ase_union.set_constraint(FixAtoms(indices=np.where(z_atoms <= z_bottom_2nd[1])[0]))
 
 ### Quantum Espresso ###
-
-command = ''
-#command='mpirun -np 4 ./pw.x -in espresso.pwi > espresso.pwo'
-
+from ase.calculators.espresso import Espresso
+calculation = input_sol["control"]["calculation"]
+kpts = input_ase["kpts"]
+command = input_ase["command"]
 if calculation=='scf':
-    calc = Espresso( pseudo_dir=pseudo_dir,
+    qe = Espresso( input_data=input_sol,
                      pseudopotentials=pseudopotentials,
-                     calculation=calculation,
                      kpts=kpts,
-                     ecutwfc=ecutwfc,
                      command=command )
 elif calculation=='relax':
-    calc = Espresso( pseudo_dir=pseudo_dir,
+    qe = Espresso( input_data=input_sol,
                      pseudopotentials=pseudopotentials,
-                     calculation=calculation,
                      kpts=kpts,
-                     ecutwfc=ecutwfc,
-                     command=command )
+                     command=command  )
 elif calculation=='bands':
-    calc = Espresso( pseudo_dir=pseudo_dir,
+    qe = Espresso( input_data=input_sol,
                      pseudopotentials=pseudopotentials,
-                     calculation=calculation,
-                     nbnd=nbands,
-                     ecutwfc=ecutwfc,
-                     command=command )
-
-a.calc = calc # add the calculator
-a.get_potential_energy() # run pw.x
+                     kpts=kpts,
+                     command=command  )
+ase_union.calc = qe
+ase_union.calc.write_input(atoms_info)
+try:
+    ase_union.get_potential_energy() # run pw.x
+except:
+    print("Calculation of get_potential_energy is not normally finished.")
+    pass
