17b45d05dfe41b3b70b2669bd9482eeb951d2c0a
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2021-01-22 12:16:28 +0900

    reformat code

--- tool/todft/to_dft.py
+++ tool/todft/to_dft.py
@@ -9,6 +9,7 @@ if len(args) != 2:
     exit(0)
 file_name = args[1]
 import toml
+
 dirs = toml.load(file_name)
 input_main = dirs["Main"]
 input_ase = dirs["ASE"]
@@ -16,6 +17,7 @@ input_sol = dirs["Solver"]
 pseudopotentials = input_sol.pop("pseudo")
 
 import pprint
+
 print("Parameters for main part")
 pprint.pprint(input_main)
 print("Parameters for ase part")
@@ -30,6 +32,7 @@ file_name = input_main["input_xyz_file"]
 wfile_name = input_main["output_file_head"]
 
 from ase.io.xyz import read_xyz, write_xyz
+
 atoms = read_xyz(open(file_name))
 atoms_info = Atoms()
 for atom in atoms:
@@ -43,7 +46,7 @@ z_min = np.min(ratoms, axis=0)[2]
 slab_size = z_max - z_min
 print('slab_size=', slab_size)
 z_margin = input_main["param"]["z_margin"]
-z_atoms = ratoms[:,2]
+z_atoms = ratoms[:, 2]
 
 z_bottom_most = [z_min - z_margin, z_min + z_margin]
 second_min_index = np.where(np.sort(z_atoms) > z_min)[0][0]
@@ -51,7 +54,7 @@ z_bottom_2nd_val = np.sort(z_atoms)[second_min_index]
 z_bottom_2nd = [z_bottom_2nd_val - z_margin, z_bottom_2nd_val + z_margin]
 
 atoms_bottom_most = Atoms()
-for idx in reversed(np.where( (z_atoms <= z_bottom_most[1]) & (z_atoms >= z_bottom_most[0]))[0]):
+for idx in reversed(np.where((z_atoms <= z_bottom_most[1]) & (z_atoms >= z_bottom_most[0]))[0]):
     atoms_bottom_most += atoms_info.pop(idx)
 ratoms_bottom_most = atoms_bottom_most.get_positions()
 ratoms_bottom_2nd = ratoms[np.where((z_atoms <= z_bottom_2nd[1]) & (z_atoms >= z_bottom_2nd[0]))]
@@ -60,16 +63,16 @@ ratoms_bottom_2nd = ratoms[np.where((z_atoms <= z_bottom_2nd[1]) & (z_atoms >= z
 r_SiH = input_main["param"]["r_SiH"]
 theta = input_main["param"]["theta"]
 
-dx = r_SiH * math.sin( math.radians(theta*0.5) )
+dx = r_SiH * math.sin(math.radians(theta * 0.5))
 dy = dx
-dz = r_SiH * math.cos( math.radians(theta*0.5) )
+dz = r_SiH * math.cos(math.radians(theta * 0.5))
 
 tmp_x_idx = np.arange(ratoms_bottom_most.shape[0])
 tmp_y_idx = np.arange(ratoms_bottom_2nd.shape[0])
 xx, yy = np.meshgrid(tmp_x_idx, tmp_y_idx)
 rxy_atoms_most = ratoms_bottom_most[:, :2]
 rxy_atoms_2nd = ratoms_bottom_2nd[:, :2]
-distances = np.linalg.norm(rxy_atoms_most[xx]-rxy_atoms_2nd[yy], axis=2)
+distances = np.linalg.norm(rxy_atoms_most[xx] - rxy_atoms_2nd[yy], axis=2)
 
 for idx, r2 in enumerate(ratoms_bottom_2nd):
     r1 = ratoms_bottom_most[np.argmin(distances[:, idx])]
@@ -78,9 +81,11 @@ for idx, r2 in enumerate(ratoms_bottom_2nd):
     if (np.abs(x12) < 1e-12 and np.abs(y12) > 1e-12) or (np.abs(x12) > 1e-12 and np.abs(y12) < 1e-12):
         print('(001)surface')
         if np.abs(x12) < 1e-12:
-            atoms_info += Atoms(numbers=[1,1], positions=[(r2[0], r2[1] - dy, r2[2] - dz), (r2[0], r2[1] + dy, r2[2] - dz)])
+            atoms_info += Atoms(numbers=[1, 1],
+                                positions=[(r2[0], r2[1] - dy, r2[2] - dz), (r2[0], r2[1] + dy, r2[2] - dz)])
         else:
-            atoms_info += Atoms(numbers=[1,1], positions=[(r2[0] - dx, r2[1], r2[2] - dz), (r2[0] + dx, r2[1], r2[2] - dz)])
+            atoms_info += Atoms(numbers=[1, 1],
+                                positions=[(r2[0] - dx, r2[1], r2[2] - dz), (r2[0] + dx, r2[1], r2[2] - dz)])
     elif np.abs(x12) < 1e-12 and np.abs(y12) < 1e-12:
         print('(111)surface')
         atoms_info += Atoms(numbers=[1], positions=[(r2[0], r2[1], r2[2] - r_SiH)])
@@ -92,43 +97,50 @@ print("natoms = {}".format(len(atoms_info)))
 #  at the last two lines
 a1 = input_main["lattice"]["unit_vec"][0]
 a2 = input_main["lattice"]["unit_vec"][1]
-a3 = np.array([0.0, 0.0, slab_size+input_main["param"]["slab_margin"]])
+a3 = np.array([0.0, 0.0, slab_size + input_main["param"]["slab_margin"]])
 atoms_info.set_cell(np.array([a1, a2, a3]))
-comment = "Lattice= \"{} {} {} {} {} {} {} {} {}\" Properties=species:S:1:pos:R:3 pbc=\"T T T\"".format(a1[0], a1[1], a1[2], a2[0], a2[1], a2[2], a3[0], a3[1], a3[2])
-write_xyz(open(wfile_name+".xyz" ,"w"), atoms_info, comment)
+comment = "Lattice= \"{} {} {} {} {} {} {} {} {}\" Properties=species:S:1:pos:R:3 pbc=\"T T T\"".format(a1[0], a1[1],
+                                                                                                        a1[2], a2[0],
+                                                                                                        a2[1], a2[2],
+                                                                                                        a3[0], a3[1],
+                                                                                                        a3[2])
+write_xyz(open(wfile_name + ".xyz", "w"), atoms_info, comment)
 from ase.io import read, write
-ase_union = read(wfile_name+".xyz")
+
+ase_union = read(wfile_name + ".xyz")
 ### CIF file ###
-ase_union.write(wfile_name+'.cif', format='cif')
+ase_union.write(wfile_name + '.cif', format='cif')
 ### Constraints ###
 from ase.constraints import FixAtoms
-z_atoms = atoms_info.get_positions()[:,2]
+
+z_atoms = atoms_info.get_positions()[:, 2]
 ase_union.set_constraint(FixAtoms(indices=np.where(z_atoms <= z_bottom_2nd[1])[0]))
 
 ### Quantum Espresso ###
 from ase.calculators.espresso import Espresso
+
 calculation = input_sol["control"]["calculation"]
 kpts = input_ase["kpts"]
 command = input_ase["command"]
-if calculation=='scf':
-    qe = Espresso( input_data=input_sol,
-                     pseudopotentials=pseudopotentials,
-                     kpts=kpts,
-                     command=command )
-elif calculation=='relax':
-    qe = Espresso( input_data=input_sol,
-                     pseudopotentials=pseudopotentials,
-                     kpts=kpts,
-                     command=command  )
-elif calculation=='bands':
-    qe = Espresso( input_data=input_sol,
-                     pseudopotentials=pseudopotentials,
-                     kpts=kpts,
-                     command=command  )
+if calculation == 'scf':
+    qe = Espresso(input_data=input_sol,
+                  pseudopotentials=pseudopotentials,
+                  kpts=kpts,
+                  command=command)
+elif calculation == 'relax':
+    qe = Espresso(input_data=input_sol,
+                  pseudopotentials=pseudopotentials,
+                  kpts=kpts,
+                  command=command)
+elif calculation == 'bands':
+    qe = Espresso(input_data=input_sol,
+                  pseudopotentials=pseudopotentials,
+                  kpts=kpts,
+                  command=command)
 ase_union.calc = qe
 ase_union.calc.write_input(atoms_info)
 try:
-    ase_union.get_potential_energy() # run pw.x
+    ase_union.get_potential_energy()  # run pw.x
 except:
     print("Calculation of get_potential_energy is not normally finished.")
     pass
