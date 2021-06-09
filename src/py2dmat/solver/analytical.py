b6652fb9cddd800b6e4e0b152ac957ac2b564117
hiwamoto <h.iwamoto.research@gmail.com>
2021-06-10 00:29:36 +0900

    add regression and regression_easy

--- src/py2dmat/solver/analytical.py
+++ src/py2dmat/solver/analytical.py
@@ -45,6 +45,35 @@ def himmelblau(xs: np.ndarray) -> float:
         )
     return (xs[0] ** 2 + xs[1] - 11.0) ** 2 + (xs[0] + xs[1] ** 2 - 7.0) ** 2
 
+def regression(xs: np.ndarray) -> float:
+    if xs.shape[0] != 3:
+        raise RuntimeError(
+            f"ERROR: regression expects d=3 input, but receives d={xs.shape[0]} one"
+        )
+
+    xdata = np.array([1, 2, 3, 4, 5, 6])
+    ydata = np.array([1, 3, 2, 4, 3, 5])
+
+    xdata = xdata - np.average(xdata)
+    ydata = ydata - np.average(ydata)
+    n = len(ydata)
+
+    return -(-0.5 * ( n*xs[2] + np.sum( (xs[0]*xdata + xs[1] - ydata)** 2 )/np.exp(xs[2]) ))
+
+def regression_easy(xs: np.ndarray) -> float:
+    if xs.shape[0] != 3:
+        raise RuntimeError(
+            f"ERROR: regression expects d=3 input, but receives d={xs.shape[0]} one"
+        )
+
+    xdata = np.array([1, 2])
+    ydata = np.array([1, 2])
+
+    #xdata = xdata - np.average(xdata)
+    #ydata = ydata - np.average(ydata)
+    n = len(ydata)
+
+    return -(- 0.5 * ( n*xs[2] + np.sum( (xs[0]*xdata + xs[1] - ydata)** 2 )/np.exp(xs[2]) ))
 
 class Solver(py2dmat.solver.function.Solver):
     """Function Solver with pre-defined benchmark functions"""
@@ -77,5 +106,11 @@ class Solver(py2dmat.solver.function.Solver):
                     f"ERROR: himmelblau works only with dimension=2 but input is dimension={dimension}"
                 )
             self.set_function(himmelblau)
+        elif function_name == "regression":
+            self.set_function(regression)
+        elif function_name == "regression_nishi":
+            self.set_function(regression_nishi)
+        elif function_name == "regression_easy":
+            self.set_function(regression_easy)
         else:
             raise RuntimeError(f"ERROR: Unknown function, {function_name}")
