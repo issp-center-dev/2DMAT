a63dbca59cdbb70f64f36d06395fe7fa86b840d8
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2021-08-05 12:25:29 +0900

    mod test

--- tests/pamc/do.sh
+++ tests/pamc/do.sh
@@ -2,7 +2,7 @@ rm -rf output
 
 time mpiexec --oversubscribe -np 2 python3 -m mpi4py ../../src/py2dmat_main.py input.toml
 
-resfile=output/fx.txt
+resfile=output/best_result.txt
 
 echo diff $resfile ref.txt
 res=0
