70c842beef435c87ceed5842fe3c177d97238329
hiwamoto <h.iwamoto.research@gmail.com>
2023-11-12 17:55:12 +0900

    Adjustment of variable types

--- src/py2dmat/util/limitation.py
+++ src/py2dmat/util/limitation.py
@@ -6,23 +6,14 @@ import py2dmat
 
 # for type hints
 from pathlib import Path
-from typing import List, Optional, TYPE_CHECKING, Dict, Tuple
+from typing import List, Optional, Dict
 
 class LimitationBase(metaclass=ABCMeta):
     @abstractmethod
-    def __init__ (self, a: Optional[np.ndarray] = None, 
-                        b: Optional[np.ndarray] = None):
-        if (a is not None) or (b is not None):
-            if a is None:
-                msg = "ERROR: b is defined but a is not."
-                raise RuntimeError(msg)
-            if b is None:
-                msg = "ERROR: a is defined but b is not."
-                raise RuntimeError(msg)
-        if (a is None) and (b is None):
-            self.islimitary = False
-        else:
-            self.islimitary = True
+    def __init__ (self, a: np.ndarray, b: np.ndarray, is_limitary: bool):
+        
+        self.islimitary = is_limitary
+        if self.islimitary:
             self.a = a
             self.b = b
             self.n_formura = a.shape[0]
@@ -33,9 +24,8 @@ class LimitationBase(metaclass=ABCMeta):
         pass
 
 class Inequality(LimitationBase):
-    def __init__ (self, a: Optional[np.ndarray] = None, 
-                        b: Optional[np.ndarray] = None):
-        super().__init__(a, b)
+    def __init__ (self, a: np.ndarray, b: np.ndarray, is_limitary: bool):
+        super().__init__(a, b, is_limitary)
 
     def judge(self, x: np.ndarray) -> bool:
         if self.islimitary :
