3d76e1ef24f2ab0047ff22c5c5e76bcdbc973bde
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-04-27 20:12:33 +0900

    modified constructor of limitation classes

--- src/py2dmat/util/limitation.py
+++ src/py2dmat/util/limitation.py
@@ -5,23 +5,22 @@ import numpy as np
 
 class LimitationBase(metaclass=ABCMeta):
     @abstractmethod
-    def __init__(self, a: np.ndarray, b: np.ndarray, is_limitary: bool):
-
+    def __init__(self, is_limitary: bool):
         self.islimitary = is_limitary
+
+    @abstractmethod
+    def judge(self, x: np.ndarray) -> bool:
+        raise NotImplementedError
+
+
+class Inequality(LimitationBase):
+    def __init__(self, a: np.ndarray, b: np.ndarray, is_limitary: bool):
+        super().__init__(is_limitary)
         if self.islimitary:
             self.a = a
             self.minusb = -b
             self.n_formura = a.shape[0]
             self.ndim = a.shape[1]
-
-    @abstractmethod
-    def judge(self, x: np.ndarray) -> bool:
-        pass
-
-
-class Inequality(LimitationBase):
-    def __init__(self, a: np.ndarray, b: np.ndarray, is_limitary: bool):
-        super().__init__(a, b, is_limitary)
 
     def judge(self, x: np.ndarray) -> bool:
         if self.islimitary:
