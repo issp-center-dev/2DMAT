5a8ca70ef29d52d2be395aca04499e493dc4f8d3
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-04-27 21:25:27 +0900

    modified limitation construction

--- src/py2dmat/util/limitation.py
+++ src/py2dmat/util/limitation.py
@@ -42,39 +42,24 @@ class Inequality(LimitationBase):
         co_a: np.ndarray = read_matrix(d.get("co_a", []))
         co_b: np.ndarray = read_matrix(d.get("co_b", []))
 
+        # is_set_co_a = (co_a.size > 0 and co_a.ndim == 2 and co_a.shape[1] == dimension)
+        # is_set_co_b = (co_b.size > 0 and co_b.ndim == 2 and co_b.shape == (co_a.shape[0], 1))
+
         if co_a.size == 0:
             is_set_co_a = False
         else:
-            is_set_co_a = True
-            if co_a.ndim != 2:
-                raise ValueError("co_a should be a matrix")
-            if co_a.shape[1] != dimension:
-                msg ='The number of columns in co_a should be equal to'
-                msg+='the value of "dimension" in the [base] section'
-                raise ValueError(msg)
-            n_row_co_a = co_a.shape[0]
+            if co_a.ndim == 2 and co_a.shape[1] == dimension:
+                is_set_co_a = True
+            else:
+                raise ValueError("co_a should be a matrix of size equal to number of constraints times dimension")
+
         if co_b.size == 0:
-            if not is_set_co_a :
-                is_set_co_b = False
-            else: # is_set_co_a is True
-                msg = "ERROR: co_a is defined but co_b is not."
-                raise ValueError(msg)
-        elif co_b.ndim == 2:
-            if is_set_co_a:
-                if co_b.shape[0] == 1 or co_b.shape[1] == 1:
-                    is_set_co_b = True
-                    co_b = co_b.reshape(-1)
-                else:
-                    raise ValueError("co_b should be a vector")
-                if co_b.size != n_row_co_a:
-                    msg ='The number of row in co_a should be equal to'
-                    msg+='the number of size in co_b'
-                    raise ValueError(msg)
-            else: # not is_set_co_a:
-                msg = "ERROR: co_b is defined but co_a is not."
-                raise ValueError(msg)
-        elif co_b.ndim > 2:
-            raise ValueError("co_b should be a vector")
+            is_set_co_b = False
+        else:
+            if co_b.ndim == 2 and co_b.shape == (co_a.shape[0], 1):
+                is_set_co_b = True
+            else:
+                raise ValueError("co_b should be a column vector of size equal to number of constraints")
 
         if is_set_co_a and is_set_co_b:
             is_limitation = True
@@ -84,4 +69,4 @@ class Inequality(LimitationBase):
             msg = "ERROR: Both co_a and co_b must be defined."
             raise ValueError(msg)
 
-        return cls(co_a, co_b, is_limitation)
+        return cls(co_a, co_b.reshape(-1), is_limitation)
